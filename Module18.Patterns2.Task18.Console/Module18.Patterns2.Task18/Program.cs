88c10553094686f6b2b8b75bd0e8663a9a6fc673
Vasilev-VV <vasiavasilev97@mail.ru>
2025-06-09 15:55:36 +0700

    Новый проект с денафицированной YoutubeDownloader

---
+++ Module18.Patterns2.Task18.Console/Module18.Patterns2.Task18/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module18.Patterns2.Task18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Module18.Patterns2.Task4.Console/Module18.Patterns2.Task4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Module18.Patterns2.Task4;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
